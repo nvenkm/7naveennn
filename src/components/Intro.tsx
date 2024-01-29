a6c7a2e6f6fff20ecc3bb70a49d27cc08f2f8401
naveen kumar <7naveennn@gmail.com>
2024-01-29 21:07:09 +0530

    Intro Section responsive,Insta Icon,Tailwind variable added

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -5,39 +5,49 @@ import Image from "next/image";
 const inter = Inter({ subsets: ["latin"] });
 import { FaGithub } from "react-icons/fa";
 import { IoLogoLinkedin } from "react-icons/io5";
+import { FaInstagram } from "react-icons/fa6";
 
 const Intro = () => {
   return (
     <section
       className={`${bebasNeue.className}  border-b border-solid border-neutral-800`}
     >
-      <div className="flex mx-28">
-        <div className="w-1/2 flex flex-col justify-center gap-10">
+      <div className="flex flex-col md:flex-row mx-4 mt-6 gap-4 md:mx-12 lg:mx-28">
+        <div className="w-full md:w-1/2 flex flex-col justify-center gap-10">
           <div>
-            <h1 className="uppercase text-white text-[101px] font-normal leading-[90.90px]">
+            <h1 className="uppercase text-white text-4xl md:text-6xl lg:text-8xl font-normal">
               hi, i am <br />
               Naveen Kumar.
             </h1>
             <p
-              className={`${inter.className} text-stone-300 text-lg font-normal leading-[27px]`}
+              className={`${inter.className} text-stone-300 text-base md:text-md font-normal leading-[27px]`}
             >
               An India based Full Stack developer passionate about building
               accessible and user friendly websites.
             </p>
           </div>
           <div className="flex items-center gap-4">
-            <button className="bg-[#D3E97A] text-[#0A0A0A] uppercase py-3 px-6 text-xl rounded-full w-fit">
+            <button className="bg-green-wala text-[#0A0A0A] uppercase py-3 px-6 text-xl rounded-full w-fit">
               Contact Me
             </button>
-            <span className="text-[#D3E97A] bg-[#0A0A0A]">
+
+            {/* Github Icon */}
+            <span className="text-green-wala bg-[#0A0A0A]">
               <FaGithub size={30} />
             </span>
-            <span className="text-[#D3E97A] bg-[#0A0A0A]">
+
+            {/* Linkedin Icon */}
+            <span className="text-green-wala bg-[#0A0A0A]">
               <IoLogoLinkedin size={32} />
+            </span>
+
+            {/* Instagram */}
+            <span className="text-green-wala bg-[#0A0A0A]">
+              <FaInstagram size={32} />
             </span>
           </div>
         </div>
-        <div className="w-1/2 flex items-center justify-center">
+        <div className="w-full md:w-1/2 flex items-center justify-center">
           <Image
             height={1024}
             width={1024}
