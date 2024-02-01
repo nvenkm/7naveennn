e379ee243545be64cc37a8d37457c06cf6ad3c01
naveen kumar <7naveennn@gmail.com>
2024-02-01 23:21:12 +0530

    Navbar animation fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { Bebas_Neue } from "next/font/google";
 import { useState } from "react";
 const bebasNeue = Bebas_Neue({ subsets: ["latin"], weight: "400" });
 import { FaBars } from "react-icons/fa";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import Link from "next/link";
 import { navItems } from "../data/Navbar";
 
@@ -18,31 +18,33 @@ const Navbar = () => {
       <nav className="flex justify-between items-center">
         <div className="text-4xl">Naveen.</div>
         <div>
-          {isOpen && (
-            <motion.ul
-              initial={{ scaleY: 0 }}
-              animate={{ scaleY: 1, transition: { duration: 0.3 } }}
-              exit={{
-                scaleY: 0,
-                transition: { duration: 0.3 },
-              }}
-              className={`flex origin-top absolute top-[91px] left-0 flex-col text-6xl bg-black-wala w-screen h-[86vh] items-center overflow-x-hidden overflow-y-hidden md:hidden gap-8`}
-            >
-              {navItems.map((item) => {
-                return (
-                  <a
-                    href="#"
-                    key={item.id}
-                    className={`hover:text-green-wala duration-300 hover:scale-105 ${
-                      item.additionalStyles ? `${item.additionalStyles}` : ""
-                    }`}
-                  >
-                    {item.title}
-                  </a>
-                );
-              })}
-            </motion.ul>
-          )}
+          <AnimatePresence>
+            {isOpen && (
+              <motion.ul
+                initial={{ scaleY: 0 }}
+                animate={{ scaleY: 1, transition: { duration: 0.3 } }}
+                exit={{
+                  scaleY: 0,
+                  transition: { duration: 0.3 },
+                }}
+                className={`flex origin-top absolute top-[91px] left-0 flex-col text-6xl bg-black-wala w-screen h-[86vh] items-center overflow-x-hidden overflow-y-hidden md:hidden gap-8`}
+              >
+                {navItems.map((item) => {
+                  return (
+                    <a
+                      href="#"
+                      key={item.id}
+                      className={`hover:text-green-wala duration-300 hover:scale-105 ${
+                        item.additionalStyles ? `${item.additionalStyles}` : ""
+                      }`}
+                    >
+                      {item.title}
+                    </a>
+                  );
+                })}
+              </motion.ul>
+            )}
+          </AnimatePresence>
           <ul
             className={`hidden md:flex md:static md:h-auto md:flex-row md:text-2xl md:w-auto gap-8 md:items-start`}
           >
