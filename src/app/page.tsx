bfcacbb2a3be18584e0887e44e18946a2647f7af
naveen kumar <7naveennn@gmail.com>
2024-02-17 20:53:14 +0530

    ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Contact from "@/components/Contact";
 import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Intro from "@/components/Intro";
 import Navbar from "@/components/Navbar";
 import Projects from "@/components/Projects";
 import ScrollToTop from "@/components/ScrollToTop";
 import Skills from "@/components/Skills";
-import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
