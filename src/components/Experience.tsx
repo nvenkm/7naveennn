316887a7dda5ed11c6d342cf9be315644e7b27dd
Naveen Kumar <7naveennn@gmail.com>
2024-08-25 16:17:20 +0530

    Update experience

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -11,6 +11,8 @@ import SectionHeading from "./SectionHeading";
 import Section from "./Section";
 import { useInView } from "react-intersection-observer";
 import { useTheme } from "@/context/ThemeContext";
+import { FaLocationArrow } from "react-icons/fa";
+import { GoLocation } from "react-icons/go";
 
 const Experience = () => {
   const { ref, inView } = useInView({
@@ -33,26 +35,37 @@ const Experience = () => {
             contentArrowStyle={{
               borderRight: "7px solid  rgb(33, 150, 243)",
             }}
-            date="Nov 2023 - present"
+            date="July 2024 - Present"
             dateClassName="dark:text-white text-black"
             iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
             icon={<MdWork />}
           >
-            <h3 className="">Full Stack Intern</h3>
-            <h4 className="">Rapidoreach,Remote</h4>
+            <h3 className="">Software Developer Intern</h3>
+            <h4 className="flex">
+              <a
+                className="underline underline-offset-2"
+                target="_blank"
+                href="https://kognics.ai"
+              >
+                Kognics.ai
+              </a>{" "}
+              <span className="flex items-center ml-3">
+                <GoLocation className="mr-1" /> Jaipur
+              </span>
+            </h4>
             <p>
-              Created their Developers API portal, Implemented Salesforce Login,
-              Created Landing Pages, Bug Fixes
+              Worked on their CRM platform, created and Integrated REST APIs,
+              Worked on MySQL database
             </p>
-            <p>React, Next.js 14, Typescript, Jotai, MongoDB</p>
+            <p>React,Javascript, Node.js, MySQL</p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
+            className=""
             visible={inView}
-            className="text-slate-300"
             contentStyle={{
               backgroundColor: "#0f172a",
             }}
-            date="July 2023 - Nov 2023"
+            date="Nov 2023 - June 2024"
             dateClassName="dark:text-white text-black"
             iconStyle={{
               background: "#0f172a",
@@ -60,10 +73,24 @@ const Experience = () => {
             }}
             icon={<MdWork />}
           >
-            <h3 className="">Foundation Course</h3>
-            <h4 className="">CodeQuotient, Mohali (Remote)</h4>
-            <p>Learned Javascript,Node.js, Socket.io, MongoDB and Much more</p>
-            <p>Created the Ticketing Management Project there</p>
+            <h3 className="">Full Stack Intern</h3>
+            <h4 className="flex">
+              <a
+                className="underline underline-offset-2"
+                target="_blank"
+                href="https://rapidoform.com"
+              >
+                Rapidoform
+              </a>{" "}
+              <span className="flex items-center ml-3">
+                <GoLocation className="mr-1" /> Remote
+              </span>
+            </h4>
+            <p>
+              Created their Developers API portal, Implemented Salesforce Login,
+              Created Landing Pages, Bug Fixes
+            </p>
+            <p>React, Next.js 14, Typescript, Jotai, MongoDB</p>
           </VerticalTimelineElement>
         </VerticalTimeline>
       </div>
